9e0102c5fcf41a31cd6a09ab34d28133eea11294
James Baker <jlbaker361@gmail.com>
2026-03-24 22:28:42 -0400

    disbale voting

--- main.py
+++ main.py
@@ -23,19 +23,26 @@ if __name__ == "__main__":
     parser=argparse.ArgumentParser()
     parser.add_argument("--testing",action="store_true")
     parser.add_argument("--disable_voting",action="store_true")
+<<<<<<< HEAD
     parser.add_argument("--immortal",action="store_true",help="no players can die")
     parser.add_argument("--disable_actions",action="store_true")
     
+=======
+>>>>>>> 92ee905 (disbale voting)
     args=parser.parse_args()
     if args.testing:
         os.environ["TESTING"] = "1"
     else:
         os.environ["TESTING"]="0"
+<<<<<<< HEAD
     
+=======
+>>>>>>> 92ee905 (disbale voting)
     if args.disable_voting:
         os.environ["VOTING"]="0"
     else:
         os.environ["VOTING"]="1"
+<<<<<<< HEAD
     
     if args.immortal:
         os.environ["IMMORTAL"]="1"
