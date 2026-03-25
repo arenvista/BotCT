fca7d9c230e86f324a94149af521892abe3fccdb
James Baker <jlbaker361@gmail.com>
2026-03-24 22:38:46 -0400

    added env

--- main.py
+++ main.py
@@ -22,13 +22,21 @@ if __name__ == "__main__":
     parser=argparse.ArgumentParser()
     parser.add_argument("--testing",action="store_true")
     parser.add_argument("--disable_voting",action="store_true")
+    parser.add_argument("--immortal",action="store_true",help="no players can die")
+    
     args=parser.parse_args()
     if args.testing:
         os.environ["TESTING"] = "1"
     else:
         os.environ["TESTING"]="0"
+    
     if args.disable_voting:
         os.environ["VOTING"]="0"
     else:
         os.environ["VOTING"]="1"
+    
+    if args.immortal:
+        os.environ["IMMORTAL"]="1"
+    else:
+        os.environ["IMMORTAL"]="0"
     main()
