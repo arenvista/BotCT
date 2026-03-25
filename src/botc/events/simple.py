f2b8deb4afd07c208cdae440dc3bc0ac4c60880c
James Baker <jlbaker361@gmail.com>
2026-03-24 20:29:40 -0400

    one shot event test

--- src/botc/events/simple.py
+++ src/botc/events/simple.py
@@ -7,6 +7,7 @@ from .base import Event,register_event,EVENT_MAP
 NULL_EVENT_ENUM="null_event"
 NULL_EVENT_CHILD_ENUM="null_event_child"
 NULL_EVENT_ANTI_CHILD_ENUM="null_event_anti_child"
+ONESHOT_EVENT_ENUM="oneshot_event"
 
 @register_event(NULL_EVENT_ENUM,[],[])
 class NullEvent(Event):
@@ -20,5 +21,10 @@ class NullEventChild(Event):
     
 @register_event(NULL_EVENT_ANTI_CHILD_ENUM,[],[NULL_EVENT_ENUM])
 class NullEventAntiChild(Event):
+    async def resolve(self,*args,**kwargs):
+        pass
+    
+@register_event(ONESHOT_EVENT_ENUM,[],[ONESHOT_EVENT_ENUM])
+class OneShotEvent(Event):
     async def resolve(self,*args,**kwargs):
         pass
