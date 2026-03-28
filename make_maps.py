bfb74dd6a14f6a700cfde598196caf092ff4fa05
James Baker <jlbaker361@gmail.com>
2026-03-28 18:33:59 -0400

    more role diversity

--- make_maps.py
+++ make_maps.py
@@ -11,10 +11,14 @@ os.makedirs("maps",exist_ok=True)
 for n in range(24):
     output={
             args.name: n,
-    "chudbotc0":10,
+    "chudbotc0":22,
     "chudbotc1":4,
     "chudbotc2":7,
-    "chudbotc3":10
+    "chudbotc3":10,
+     "chudbotc4":2,
+         "chudbotc5":8,
+    "chudbotc6":13,
+    "chudbotc7":4,
     }
     try:
         print(RoleName(n).display_name)
